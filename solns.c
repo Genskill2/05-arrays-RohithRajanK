7a24a1d2db43ed5f487257ddf9ea46c2f734a064
RohithRajanK <84078186+RohithRajanK@users.noreply.github.com>
2021-06-19 10:08:48 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,127 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+//maximum
+
+int max(int a[],int b)
+{
+int big=a[0];
+
+for(int i=o;i<b;i++)
+{
+if(a[i]>big)
+{
+big=a[i];
+}
+}
+return(big);
+}
+
+//minimum
+
+int max(int a[],int b)
+{
+int small=a[0];
+
+for(int i=o;i<b;i++)
+{
+if(a[i]<big)
+{
+small=a[i];
+}
+}
+return(small);
+}
+//average
+
+float average(int a[], int b)
+{
+float avg=0;
+for(int i=0;i<b;i++)
+{
+avg=avg+a[i];
+}
+return(avg);
+}
+
+
+//mode
+
+int mode(int a[], int b)
+{
+int big=a[0];
+
+for(int i=o;i<b;i++)
+{
+if(a[i]>big)
+{
+big=a[i];
+}
+}
+int count[big+1];
+for(int j=0;i<big+1;j++)
+{
+count[j]=0;
+for(int i=0;i<big+1;i++)
+{
+if(a[i]==j)
+{
+count[j]=count[j]+1;
+}
+}
+}
+int big1;
+for(int i=o;i<big+1;i++)
+{
+if(count[i]>big1)
+{
+big1=count[i];
+}
+}
+int value;
+for(int i=o;i<big+1;i++)
+{
+if(count[i]==big1)
+{
+value=i;
+break;
+}
+}
+return(value);
+
+
+
+
+
+//prime factor
+int factors(int n, int f[])
+{
+	
+	int r=n,p=0,i=2;
+	while(r!=1)
+	{
+		int flag=0;
+		for(int j=2;j<=i/2;j++)
+		{
+			if(i%j==0)
+			{
+				flag=1;
+				break;
+			}
+				
+		}
+		if (flag==0)
+		{
+			while(r%i==0)
+			{
+				r=r/i;
+				f[p]=i;
+				p++;
+			}
+				
+			
+		}
+		i++;
+	}
+	//return f;
+	return p;
+}
